719b6e7c3a7373865d7b1ef0058baa82a27540c4
brettdoyle44@gmail.com <h0ck3ypl@y3r44>
2019-12-03 23:22:47 -0500

    Add create event functionality

---
+++ create.js
@@ -0,0 +1,27 @@
+import uuid from "uuid";
+import * as dynamoDbLib from "./libs/dynamodb-lib";
+import { success, failure } from "./libs/response-lib";
+
+export function main(event, context, callback) {
+  const data = JSON.parse(event.body);
+
+  const params = {
+    TableName: process.env.tableName,
+    Item: {
+      userid: event.requestContext.identity.cognitoIdentityId,
+      eventid: uuid.v1(),
+      title: data.title,
+      content: data.content,
+      startDate: data.startDate,
+      image: data.image,
+      createdAt: Date.now()
+    }
+  };
+
+  try {
+    await dynamoDbLib.call("put", params);
+    return success(params.Item);
+  } catch (e) {
+    return failure({ status: false });
+  }
+}
