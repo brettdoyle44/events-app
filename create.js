36bfe81a0108164596c4fdd51683ce135f52037e
brettdoyle44@gmail.com <h0ck3ypl@y3r44>
2019-12-15 10:59:49 -0500

    Set up Serverless infrastructure

--- create.js
+++ create.js
@@ -1,8 +1,8 @@
-import uuid from "uuid";
-import * as dynamoDbLib from "./libs/dynamodb-lib";
-import { success, failure } from "./libs/response-lib";
+import uuid from 'uuid';
+import * as dynamoDbLib from './libs/dynamodb-lib';
+import { success, failure } from './libs/response-lib';
 
-export function main(event, context, callback) {
+export async function main(event, context, callback) {
   const data = JSON.parse(event.body);
 
   const params = {
@@ -19,7 +19,7 @@ export function main(event, context, callback) {
   };
 
   try {
-    await dynamoDbLib.call("put", params);
+    await dynamoDbLib.call('put', params);
     return success(params.Item);
   } catch (e) {
     return failure({ status: false });
